72d8165f3037880b18c49f3141e4ee76be557dd7
SayliHadke <saylihadke21@gmail.com>
2024-12-15 12:22:11 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace BMI_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
